c7b5d963e9ede15ed04e8de6289992e45b3f7bd0
LyamAL <zlove750@163.com>
2019-10-11 20:28:10 +0800

    更新Client

--- ssncv-config-client/src/main/java/com/zaq/ssncv/ssncvconfigclient/ClientApplication.java
+++ ssncv-config-client/src/main/java/com/zaq/ssncv/ssncvconfigclient/ClientApplication.java
@@ -1,4 +1,14 @@
 package com.zaq.ssncv.ssncvconfigclient;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ZAQ
+ */
+@SpringBootApplication
 public class ClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+    }
 }
--- ssncv-config-client/src/main/java/com/zaq/ssncv/ssncvconfigclient/controller/TestController.java
+++ ssncv-config-client/src/main/java/com/zaq/ssncv/ssncvconfigclient/controller/TestController.java
@@ -1,4 +1,28 @@
 package com.zaq.ssncv.ssncvconfigclient.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ZAQ
+ */
+@RefreshScope
+@RestController
 public class TestController {
+    private Environment env;
+
+    public TestController(@Autowired Environment env) {
+        this.env = env;
+    }
+
+    //    @Value("${from}")
+//    private String from;
+    @RequestMapping("/from")
+    public String from() {
+        return env.getProperty("from", "undefined");
+    }
 }
+
